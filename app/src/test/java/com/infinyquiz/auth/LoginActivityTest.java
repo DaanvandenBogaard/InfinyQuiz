e32909d4a0471bf3dc95506e8fca93ea557f929a
DaanBogaard <d.c.l.v.d.bogaard@student.tue.nl>
2022-02-24 14:41:38 +0100

    Added files for unit testing of loginActivity and RegisterActivity

---
+++ app/src/test/java/com/infinyquiz/auth/LoginActivityTest.java
@@ -0,0 +1,12 @@
+package com.infinyquiz.auth;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginActivityTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
---
+++ app/src/test/java/com/infinyquiz/auth/RegisterActivityTest.java
@@ -0,0 +1,12 @@
+package com.infinyquiz.auth;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RegisterActivityTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
