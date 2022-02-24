3702f3628ccf02d06777f468b93584fb0aaf2e65
DaanBogaard <d.c.l.v.d.bogaard@student.tue.nl>
2022-02-24 21:44:55 +0100

    Got basics for starting the instrumented tests in.

--- app/src/androidTest/java/com/infinyquiz/auth/LoginActivityTest.java
+++ app/src/androidTest/java/com/infinyquiz/auth/LoginActivityTest.java
@@ -1,7 +1,22 @@
 package com.infinyquiz.auth;
 
 
-public class LoginActivityTest {
+import android.support.test.rule.ActivityTestRule;
 
+import org.junit.Before;
+import org.junit.Rule;
+
+public class LoginActivityTest {
+    //Code to run activity during test:
+    @Rule
+    public ActivityTestRule<LoginActivity> registerActivtyRule = new ActivityTestRule<>(LoginActivity.class);
+
+    //The activity itself
+    private LoginActivity activity;
+
+    @Before
+    public void initTestEnvironement() {
+        activity = registerActivtyRule.getActivity();
+    }
 }
 
