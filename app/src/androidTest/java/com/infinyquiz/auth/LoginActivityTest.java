a155b991aebedfae52f40d1607e42d2264fc87ff
DaanBogaard <d.c.l.v.d.bogaard@student.tue.nl>
2022-02-24 15:15:13 +0100

    Added files for instrumented tests.

---
+++ app/src/androidTest/java/com/infinyquiz/auth/LoginActivityTest.java
@@ -0,0 +1,4 @@
+package com.infinyquiz.auth;
+
+public class LoginActivityTest {
+}
--- app/src/test/java/com/infinyquiz/auth/RegisterActivityTest.java
+++ app/src/test/java/com/infinyquiz/auth/RegisterActivityTest.java
@@ -4,7 +4,13 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/*
+ * We do not have methods that we can unit test.
+ * Instead, we have instrumented tests for these.
+ */
 public class RegisterActivityTest {
+
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
