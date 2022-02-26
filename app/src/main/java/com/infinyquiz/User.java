5d2117f5bb9be1402b71f67a988b0daa8c343975
MikevdAnker <mikevdanker@gmail.com>
2022-02-26 15:36:02 +0100

    Added elements and basic functions such as getters as described in the UML to User class.

--- app/src/main/java/com/infinyquiz/User.java
+++ app/src/main/java/com/infinyquiz/User.java
@@ -1,9 +1,17 @@
 package com.infinyquiz;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.List;
+
 public class User {
 
-    public String username, mail;
+    public String username, mail, id;
+    public List<String> friendList;
+    public long profilePictureID;
 
+    //This empty constructor is necessary for Firebase to work
     public User() {
 
     }
@@ -11,6 +19,34 @@ public class User {
     public User(String username, String mail) {
         this.username = username;
         this.mail = mail;
+        this.id = FirebaseAuth.getInstance().getUid();
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void addFriend(String friendID) {
+        friendList.add(friendID);
+    }
+
+    public void removeFriend(String friendID) {
+        friendList.remove(friendID);
+    }
+
+    public void setProfilePicture(long profilePictureID) {
+        this.profilePictureID = profilePictureID;
+    }
+
+    public long getProfilePictureID() {
+        return profilePictureID;
+    }
 }
