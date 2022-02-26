00f0db2194270970cd479535927c223f1970fce3
DaanBogaard <d.c.l.v.d.bogaard@student.tue.nl>
2022-02-26 12:40:03 +0100

    Deleted unused imports

--- app/src/main/java/com/infinyquiz/HomeActivity.java
+++ app/src/main/java/com/infinyquiz/HomeActivity.java
@@ -1,16 +1,8 @@
 package com.infinyquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-import com.infinyquiz.OnClickListener.MoveToActivityOnClickListener;
-import com.infinyquiz.auth.LoginActivity;
-import com.infinyquiz.auth.RegisterActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
