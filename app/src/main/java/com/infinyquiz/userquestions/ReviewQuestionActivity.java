11e484b7a89c838996dc24bb96a88f0454185b8b
DaanBogaard <d.c.l.v.d.bogaard@student.tue.nl>
2022-03-02 16:42:57 +0100

    Added ReviewQuestionActivity and added this to manifest. Also created (empty) XML file for this activity.

---
+++ app/src/main/java/com/infinyquiz/userquestions/ReviewQuestionActivity.java
@@ -0,0 +1,17 @@
+package com.infinyquiz.userquestions;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.infinyquiz.R;
+import com.infinyquiz.onclicklistener.MoveToActivityOnClickListener;
+
+public class ReviewQuestionActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reviewquestion);
+
+    }
+}
