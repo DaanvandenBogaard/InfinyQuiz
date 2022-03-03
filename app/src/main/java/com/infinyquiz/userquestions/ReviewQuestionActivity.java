16fefcb02cb4f7b1142e80d9df43cc474d7db7a1
DaanBogaard <d.c.l.v.d.bogaard@student.tue.nl>
2022-03-03 19:33:14 +0100

    Fixed all issues we had with regards to retrieving the random questions to be satisfied from the database. Todo next: implement Ui handling. After this, split into hardware work (camera options), matchmaking and user stats.

--- app/src/main/java/com/infinyquiz/userquestions/ReviewQuestionActivity.java
+++ app/src/main/java/com/infinyquiz/userquestions/ReviewQuestionActivity.java
@@ -1,35 +1,77 @@
 package com.infinyquiz.userquestions;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.infinyquiz.HomeActivity;
-import com.infinyquiz.QuestionRetriever;
 import com.infinyquiz.R;
 import com.infinyquiz.datarepresentation.Question;
 import com.infinyquiz.onclicklistener.MoveToActivityOnClickListener;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class ReviewQuestionActivity extends AppCompatActivity {
+
+    private boolean hasRetrievedQuestion = false;
+    private Question question = new Question();
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reviewquestion);
 
         Button backToHomeBtn = (Button) findViewById(R.id.backToHomeBtn);
-        backToHomeBtn.setOnClickListener(new MoveToActivityOnClickListener(new HomeActivity(),this));
+        backToHomeBtn.setOnClickListener(new MoveToActivityOnClickListener(new HomeActivity(), this));
 
-        QuestionRetriever qR = new QuestionRetriever(this);
-        Question question = qR.getReviewQuestion();
-
-        TextView debugTV = findViewById(R.id.debugTV);
+        getQuestion();
     }
 
-    public Question getRandomQuestion(){
-        //Make connection to database:
+    //Method to get question data
+    //Also sends it to setQuestion method, as
+    private void getQuestion() {
+        DatabaseReference ref = FirebaseDatabase.getInstance("https://infinyquiz-a135e-default-rtdb.europe-west1.firebasedatabase.app/").getReference().child("NotValidatedQuestions");
+        // Attach a listener to read the data at our posts reference
+        ArrayList<Question> questions = new ArrayList<>();
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(hasRetrievedQuestion){
+                    return;
+                }
+                hasRetrievedQuestion = true;
 
-        //Retrieve random question from database:
-        return new Question();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Question question = snapshot.getValue(Question.class);
+                    questions.add(question);
+                }
+                //pick random question
+                question = questions.get(new Random().nextInt(questions.size()));
+                setQuestion(question);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("could not read data", "The read failed: " + databaseError.getCode());
+            }
+        });
     }
+
+    //sets the correct question into UI;
+    private void setQuestion(Question question){
+        //TODO: provide correct implementation
+        System.out.println(question);
+        System.out.println(question.getQuestion());
+        System.out.println(question.getCorrectOption());
+        System.out.println(question.getOptions());
+    }
+
 }
