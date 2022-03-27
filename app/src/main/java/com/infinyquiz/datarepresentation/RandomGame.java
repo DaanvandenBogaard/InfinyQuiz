8b8a09b962058629e130d257f199137ca3a65b66
DaanBogaard <d.c.l.v.d.bogaard@student.tue.nl>
2022-03-27 23:28:57 +0200

    Added more documentation to RandomGame.java

--- app/src/main/java/com/infinyquiz/datarepresentation/RandomGame.java
+++ app/src/main/java/com/infinyquiz/datarepresentation/RandomGame.java
@@ -2,18 +2,32 @@ package com.infinyquiz.datarepresentation;
 
 import java.util.ArrayList;
 
+/* A concrete class implementing the abstract class {@code Game.class}
+ *
+ * Note that this is the only concrete class implementing {@code Game.class} due to our
+ * time constraints. Also note that we did not have a "must" requirement that stated
+ * that we needed to implement more.
+ *
+ * The class is quite minimalistic. Originally the plan was to have the custom game
+ * where the selection of questions would be very different from where it concerns a
+ * regular game.
+ *
+ * This class is used to represent a game which came to be due to random matchmaking.
+ */
 public class RandomGame extends Game{
 
-
+    //Constructor
     public RandomGame(Lobby lobby) {
         super(lobby);
     }
 
-    //Constructor for firebase
+    //Constructor for firebase (needs to be empty)
     public RandomGame(){
         super();
     }
 
+    //Originally we had different plans to do this method. In the end, due to time
+    //constraints we went with the easier approach.
     @Override
     public void setQuestions(ArrayList<Question> _questions) {
         questions = _questions;
