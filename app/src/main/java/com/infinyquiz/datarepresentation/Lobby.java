b5c1fb07afc0e9a4ed53404684c02bc394eac054
DaanBogaard <d.c.l.v.d.bogaard@student.tue.nl>
2022-03-27 23:16:21 +0200

    Added more documentation to the Lobby class.

--- app/src/main/java/com/infinyquiz/datarepresentation/Lobby.java
+++ app/src/main/java/com/infinyquiz/datarepresentation/Lobby.java
@@ -6,6 +6,11 @@ import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Map;
 
+/* The data representation of a lobby object.
+ *
+ * Used stand alone in firebase and {@code MatchMaker}.
+ * Used via composition in {@code Game}.
+ */
 public class Lobby {
 
     //Constant: maximal number of people in lobby
@@ -26,47 +31,71 @@ public class Lobby {
     //Boolean saying whether the game has started.
     private boolean gameHasStarted = false;
 
-    //Constructor
-    public Lobby(){
-
+    //Empty constructor, needed by firebase.
+    public Lobby() {
     }
 
-    public void addUser(String id){
+    /* A function to add the id of a user to the list of user ids.
+     *
+     * @param String id, the id of the user that we want to add to the {@code users} list.
+     * @pre {@code id != null && !id.equals("") }
+     * @post {@code users.contains(id) && !users.contains(null)}
+     * @throws none
+     * @modifies {@code users}
+     */
+    public void addUser(String id) {
         users.add(id);
         users.removeAll(Collections.singleton(null));
     }
 
-    public ArrayList<String> getUsers(){
+    /* A function to get the ArrayList of user ids.
+     *
+     * @pre {@code users != null}
+     * @post {@code !users.contains(null)}
+     * @throws none
+     * @returns {@code users}
+     * @modifies {@code users}
+     */
+    public ArrayList<String> getUsers() {
         users.removeAll(Collections.singleton(null));
         return users;
     }
 
-    public int getLobbySize(){
+    /* A function to retrieve the number of users registered in this lobby
+     *
+     * @pre {@code users != null}
+     * @post {@code !users.contains(null)}
+     * @returns {@code users.size()}
+     */
+    public int getLobbySize() {
         users.removeAll(Collections.singleton(null));
         return users.size();
     }
 
-    public String getId(){
+    /* Standard getters and setters.
+     * Contracts are implied.
+     */
+    public String getId() {
         return ID;
     }
 
-    public void setID(String id){
+    public void setID(String id) {
         ID = id;
     }
 
-    public void setGameID(String id){
+    public void setGameID(String id) {
         gameID = id;
     }
 
-    public String getGameID(){
+    public String getGameID() {
         return gameID;
     }
 
-    public boolean gameHasStarted(){
+    public boolean gameHasStarted() {
         return gameHasStarted;
     }
 
-    public void shutDownLobby(){
+    public void shutDownLobby() {
         gameHasStarted = true;
     }
 
